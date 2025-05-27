52d11edd71412e75130e91728e06a5a415a96f81
brandonsu1622 <brandon.su1622@gmail.com>
2025-05-27 09:23:48 -0400

    testing

--- cptbrandon.java
+++ cptbrandon.java
@@ -1,11 +1,30 @@
 import arc.*; 
 
 public class cptbrandon{ 
+		//global variables 
+		static String strname = ""; 
+		static boolean blncheat = false;
 	public static void main(String [] args){ 
-		Console con = new Console(); 
-		//skz related
-		//how many questions? 		
-		con.println("welcome to chans choice"); 
+		Console con = new Console("Multiple choice", 1280, 720); 	
+		
+		//main menu 
+		while(true){ 
+			con.drawString("Welcome to Math Training Game", 280, 50);
+			
+			con.drawString("What would you like to do?", 450, 120);
+			
+			con.drawString("Play Game", 480, 320);
+
+			con.drawString("Leaderboard", 480, 400);
+			
+			con.drawString("Help", 480, 480);
+			
+			con.drawString("Quit", 480, 560);
+		}
+			
+			
+	
+		 
 		
 		
 		
