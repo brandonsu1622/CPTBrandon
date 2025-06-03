c7ad90d3e5d7755fdf511cc7c246dc3a1018045d
brandonsu1622 <brandon.su1622@gmail.com>
2025-06-03 08:44:26 -0400

    adding text files

--- cptbrandon.java
+++ cptbrandon.java
@@ -1,4 +1,8 @@
 import arc.*; 
+import java.awt.image.*;
+import java.awt.Font;
+import java.awt.Color;
+
 
 public class cptbrandon{ 
 		//global variables 
@@ -6,10 +10,16 @@ public class cptbrandon{
 		static boolean blncheat = false;
 	public static void main(String [] args){ 
 		Console con = new Console("Multiple choice", 1280, 720); 	
+		// Main Screen of the Multiple Choice Game
+		while(true){
+			
+			// Creating a background image
 		
 		//main menu 
+
+		
 		while(true){ 
-			con.drawString("Welcome to Math Training Game", 280, 50);
+			con.drawString("Welcome to Multiple Choice", 280, 50);
 			
 			con.drawString("What would you like to do?", 450, 120);
 			
@@ -20,17 +30,26 @@ public class cptbrandon{
 			con.drawString("Help", 480, 480);
 			
 			con.drawString("Quit", 480, 560);
+			break;
 		}
+		System.out.println("Able to display to main screen");			
 			
-			
+		//Mouse iput 
+		
+		while(true){
+				if(con.currentMouseButton() == 1) {
+					// Finding the location of the mouse if it pressed one of the buttons on the screen
+					int intMouseX = con.currentMouseX();
+					int intMouseY = con.currentMouseY();				
+					// The Play Game Screen 
+					if(intMouseX >= 400 && intMouseX <= 780 && intMouseY >= 320 && intMouseY <= 370){
+						TextInputFile quizes = new TextInputFile("quizes.txt");
+						
+						con.clear();
+					}
+				}
+			}
 	
-		 
-		
-		
-		
-		
-		
-		
-		
 	}
+}
 }
