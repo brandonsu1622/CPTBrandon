79bb684dafda90bbaf8618f69833a3176c08a076
brandonsu1622 <brandon.su1622@gmail.com>
2025-06-06 08:19:12 -0400

    Update MultipleChoiceTest.java

--- MultipleChoiceTest.java
+++ MultipleChoiceTest.java
@@ -7,8 +7,7 @@ public class MultipleChoiceTest{
 	public static void quiz1(Console con){ 
 		TextInputFile straykids = new TextInputFile("straykids.txt"); 
 		
-		//array 
-		
+		//basic array 
 		String strstraykids [][];
 		
 		//variables
@@ -27,6 +26,7 @@ public class MultipleChoiceTest{
 		int intnum = 0; 
 		double dblavg = 0; 
 		
+		strname = cptbrandon.strname; 
 		//counting number of questions
 		
 		while(straykids.eof() == false){
@@ -41,10 +41,57 @@ public class MultipleChoiceTest{
 			intquestions += 1; 
 		} 
 		straykids.close(); 	
-		con.println("total questions" +intquestions); 
-	}
+	
+		//making an array 
+		
+		
+		strstraykids = new String [intquestions][7];
+		while(straykids.eof() == false){
+			String strrandnum; 
+			int introw; 
 			
-
-
+			//rows and columns 
+			
+			for(introw = 0; introw < intquestions; introw++){ 
+				
+				strrandnum = Integer.toString((int)(Math.random() * 100 + 1));
+				
+				strstraykids[introw][0] = straykids.readLine(); 
+				strstraykids[introw][1] = straykids.readLine(); 
+				strstraykids[introw][2] = straykids.readLine(); 
+				strstraykids[introw][3] = straykids.readLine(); 
+				strstraykids[introw][4] = straykids.readLine(); 
+				strstraykids[introw][5] = straykids.readLine(); 
+				strstraykids[introw][6] = strrandnum; 
+			}
+		}
+		
+		straykids.close(); 	
+		
+		
+		//bubble sort 
+		
+		String strtempquestion; 
+		String strtempans1; 
+		String strtempans2;
+		String strtempans3; 
+		String strtempans4; 
+		String strtempans; 
+		String strrand; 
+		int introw1; 
+		int introw2; 
+		
+		for(introw2 = 0; introw2 > intquestions -1; introw2++){ 
+			 for(introw1 = 0; introw1 > intquestions - 1 - introw2 ; introw2++){ 
+				 //convertig string into int and comaparing them 
+				 if(Integer.parseInt(strstraykids[introw1][6]) > Integer.parseInt(strstraykids[introw1 + 1][6])){ 
+					 strtempquestion = strstraykids[introw1][0]; 
+				 }
+				 
+			 }
+		 }
+	 }
+		
+		
 
 }
