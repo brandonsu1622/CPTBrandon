267127d6f0a27534f0c6074c29f2caadc2d47937
brandonsu1622 <brandon.su1622@gmail.com>
2025-06-12 11:26:15 -0400

    debugging

--- cptbrandon.java
+++ cptbrandon.java
@@ -1,6 +1,10 @@
+//Brandon Su 
+//Multiple Choice quiz
+//Version 3.16 
+//last edit: June 12th 2025 
+
 import arc.*; 
 import java.awt.image.*;
-import java.awt.Font;
 import java.awt.Color;
 
 
@@ -8,6 +12,7 @@ public class cptbrandon{
 		//global variables 
 		static String strname = ""; 
 		static boolean blncheat = false;
+		static String strquiz = ""; 
 	public static void main(String [] args){ 
 		Console con = new Console("Multiple choice", 1280, 720); 	
 		// Main Screen
@@ -38,7 +43,10 @@ public class cptbrandon{
 			con.repaint(); 
 			
 		
-		System.out.println("Able to display to main screen");			
+		System.out.println("Able to display to main screen");		
+		
+		
+			
 			
 		//Mouse iput 
 		
@@ -61,27 +69,213 @@ public class cptbrandon{
 						strname = con.readLine();
 						con.clear();
 						
-						con.println("Hello " + strname+" what quiz would you like to do?");
+						con.println("Hello " + strname);
 						con.sleep(1000);
 						
 						
-						con.drawString("Return to Main Menu", 510, 575);
 
 						int intX = 120;
 						
 						while(quizes.eof() == false){
-							con.drawString(quizes.readLine(), intX, 420);
+							con.drawString(quizes.readLine(), intX, 300);
 							intX += 300;
-							
+					
 							
 						}
 					con.println("what quiz would you like to play?"); 
 					String strquiz = con.readLine(); 
 					
+	
+		// accessing the added quiz
+		String filename = strquiz + ".txt";
+	if(strquiz.equalsIgnoreCase(filename)){
+		TextInputFile newquiz = new TextInputFile(filename);
+		
+		//basic array 
+		String strquiz2 [][];
+
+		
+		//variables
+		String strquestion = ""; 
+		String stranswer1 = ""; 
+		String stranswer2 = ""; 
+		String stranswer3 = ""; 
+		String stranswer4 = ""; 
+		String stranswer = ""; 
+		String strname = ""; 
+		
+		//Initializing variables 
+		
+		int intquestions = 0; 
+		int intcorrectanswer = 0; 
+		int intnum = 0; 
+		double dblavg = 0; 
+		
+		
+		//counting number of questions
+		
+		while(newquiz.eof() == false){
+			
+			strquestion = newquiz.readLine(); 
+			stranswer1 = newquiz.readLine(); 
+			stranswer2 = newquiz.readLine(); 
+			stranswer3 = newquiz.readLine(); 
+			stranswer4 = newquiz.readLine(); 
+			stranswer = newquiz.readLine(); 
+			
+			intquestions += 1; 
+		} 
+		newquiz.close(); 	
+	
+		//making an array 
+		
+		TextInputFile quiz = new TextInputFile(filename);
+		strquiz2 = new String [intquestions][7];
+		while(newquiz.eof() == false){
+			String strrandnum; 
+			int introw; 
+			
+			//rows and columns 
+			
+			for(introw = 0; introw < intquestions; introw++){ 
+				
+				strrandnum = Integer.toString((int)(Math.random() * 100 + 1));
+				
+				strquiz2[introw][0] = quiz.readLine(); 
+				strquiz2[introw][1] = quiz.readLine(); 
+				strquiz2[introw][2] = quiz.readLine(); 
+				strquiz2[introw][3] = quiz.readLine(); 
+				strquiz2[introw][4] = quiz.readLine(); 
+				strquiz2[introw][5] = quiz.readLine(); 
+				strquiz2[introw][6] = strrandnum; 
+			}
+		}
+		
+		newquiz.close(); 	
+		
+		
+		//bubble sort 
+		 quiz = new TextInputFile(filename);
+		
+		
+		//temperory variables 
+		String strtempquestion; 
+		String strtempans1; 
+		String strtempans2;
+		String strtempans3; 
+		String strtempans4; 
+		String strtempans; 
+		String strrand; 
+		int introw1; 
+		int introw2; 
+		
+		for(introw2 = 0; introw2 < intquestions - 1; introw2++) {
+			
+			for(introw1 = 0; introw1 < intquestions - 1 - introw2; introw1++) { 
+				 //convertig string into int and comaparing them 
+				 
+				 if(Integer.parseInt(strquiz2[introw1][6]) > Integer.parseInt(strquiz2[introw1 + 1][6])){
+					 
+					 
+					 strtempquestion = strquiz2[introw1][0];
+					 strtempans1 = strquiz2[introw1][1]; 
+					 strtempans2 = strquiz2[introw1][2];  
+					 strtempans3 = strquiz2[introw1][3]; 
+					 strtempans4 = strquiz2[introw1][4];
+					 strtempans = strquiz2[introw1][5];
+					 strrand = strquiz2[introw1][6];
+					 
+					 strquiz2[introw1][0] = strquiz2[introw1 + 1][0];
+					 strquiz2[introw1][1] = strquiz2[introw1 + 1][1]; 
+					 strquiz2[introw1][2] = strquiz2[introw1 + 1][2];  
+					 strquiz2[introw1][3] = strquiz2[introw1 + 1][3]; 
+					 strquiz2[introw1][4] = strquiz2[introw1 + 1][4];
+					 strquiz2[introw1][5] = strquiz2[introw1 + 1][5];
+					 strquiz2[introw1][6] = strquiz2[introw1 + 1][6];
+					
+					 strquiz2[introw1 + 1][0] = strtempquestion;				
+					 strquiz2[introw1 + 1][1] = strtempans1; 
+					 strquiz2[introw1 + 1][2] = strtempans2; 
+					 strquiz2[introw1 + 1][3] = strtempans3; 
+					 strquiz2[introw1 + 1][4] = strtempans4; 
+					 strquiz2[introw1 + 1][5] = strtempans; 
+					 strquiz2[introw1 + 1][6] = strrand; 
+					 
+				 }  
+			 }
+		 }
+		 //printing the tests
+		 int intloop; 
+		 
+		 //printing the answers 
+		 
+		 for(intloop = 0; intloop < intquestions; intloop++){
+			 dblavg = ((double) intcorrectanswer /(double) intquestions) * 100;
+			 con.println(strname + "  "+filename+"   " + dblavg + "%");
+			 con.println("Question " + strquiz2[intloop][0]); 
+			 con.println("A. " + strquiz2[intloop][1]);
+			 con.println("B. " + strquiz2[intloop][2]);
+			 con.println("C. " + strquiz2[intloop][3]);
+			 con.println("D. " + strquiz2[intloop][4]);
+
+			 stranswer = con.readLine();
+			 
+			 //checking the answer 
+			 if(stranswer.equalsIgnoreCase(strquiz2[intloop][5])) {
+				intcorrectanswer++;
+				con.println("Correct!");
+			} else {
+					con.println("Wrong! The correct answer was " + strquiz2[intloop][5]);
+				}
+				
+				
+			con.sleep(1000);
+			con.clear(); 
+		 }
+			dblavg = ((double) intcorrectanswer /(double) intquestions) * 100;
+			con.println("You got " + intcorrectanswer + "/" + intquestions + " correct.");
+			con.println("Score: " + dblavg + "%");
+
+			System.out.println("able to run new quiz");  
+							for (introw1 = 0; introw1 < intquestions; introw1++) {
+								for (introw2 = 0; introw2 < 6; introw2++) {
+								String line = quiz.readLine();
+							if (line == null) {
+								System.out.println("File ended unexpectedly at question " + (introw1+1));
+							break;
+							}
+						strquiz2[introw1][introw2] = line;
+						
+						}
+						strquiz2[introw1][6] = Integer.toString((int)(Math.random() * 100 + 1));
+						}
+						
+			//leaderboard
+			TextOutputFile leaderboard = new TextOutputFile("leaderboard.txt", true); 
+			
+			leaderboard.println(strname); 
+			leaderboard.println("valorant"); 
+			leaderboard.println(dblavg); 
+			
+			//result screen 
+			con.println("Final Results: ");
+			con.println("Name:       " +strname);
+			con.println("Test type: 	    "+strquiz);
+			con.println("Win Percentage: " + dblavg + "%");
+					
+			con.sleep(2000);
+			con.println("Thanks for playing!");
+			con.println("Return back to main menu(yes or no)?");
+			String strback = con.readLine();
+			if(strback.equalsIgnoreCase("yes")){
+				con.clear();
+			}
+			con.clear();
+					}	 
 						//accessing the quizes
 						//stray kids quiz
 					
-						if(strquiz.equalsIgnoreCase("straykids")){
+					if(strquiz.equalsIgnoreCase("straykids")){
 							TextInputFile straykids = new TextInputFile(strquiz);
 							con.clear();  
 							con.drawImage(imgbackground, 0, 0);
@@ -122,7 +316,7 @@ public class cptbrandon{
 							
 							
 							
-						}
+						} 
 					
 				}
 			
@@ -256,7 +450,7 @@ public class cptbrandon{
 				//asking and reading quiz name
 				con.println("----add new quiz---"); 
 				con.println("enter quiz name(no spaces, it will be saved as a .txt file)"); 
-				String strquiz = con.readLine();
+				strquiz = con.readLine();
 				TextOutputFile newquiz = new TextOutputFile(strquiz + ".txt"); 
 				 
 				
@@ -300,6 +494,21 @@ public class cptbrandon{
 						
 						break; 					
 			}
+			// The Quit Screen of the Math Training Game
+					else if(intMouseX >= 500 && intMouseX <= 780 && intMouseY >= 640 && intMouseY <= 690){ 
+						
+						con.clear();
+						con.drawImage(imgbackground, 0, 0);
+						
+						// Debugging
+						System.out.println("Able to access Quit Menu");	
+						
+						con.println("Thank you for playing! See you next time!");
+						con.sleep(3000);
+						con.closeConsole();
+						
+					}
+					
 					
 				}
 			}
